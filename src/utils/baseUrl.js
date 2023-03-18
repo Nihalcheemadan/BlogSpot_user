1461bb5aa3e190b96a8d0f88cb19a99aec10615b
Nihal <nihalcheemadan12345@gmail.com>
2023-03-18 10:06:43 +0530

    baseUrl changed

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "https://www.server.blogsspot.site/api"
-        baseURL: "http://localhost:5000/api"
+    baseURL: "https://www.server.blogsspot.site/api"
+        // baseURL: "http://localhost:5000/api"
 
 })
 
